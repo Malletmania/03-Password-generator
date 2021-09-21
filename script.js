2f68ee14951880c01b8d2c320316776dc83b9a76
Ben Camarillo <jtby97@gmail.com>
2021-09-20 22:51:31 -0500

    added structure to javascript

--- script.js
+++ script.js
@@ -1,78 +1,80 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numberOfCharacters = 16;
-var specialCharacters = true;
-var numericConfirm = true;
-var lowerCaseConfirm = true;
-var upperCaseConfirm = true;
-var result = arr[Math.floor(Math.random() * arr.length)];
-// console.log(Array.prototype);
-// var finalPassword = window.confirm("Need a new password?")
-
-// if( finalPassword === true ){
-//    var newPassword = window.prompt("Enter password below")
-//     console.log(newPassword);
-// }
-// else {
-//   console.log (typeof password)
-// }
-// return finalPassword;
-
-  function askHowManyCharacters() {
-    var characterCount = window.confirm("How many characters would you like your password to contain")
-    console.log(characterCount)
-    return characterCount;
+var passwordMin = 8;
+var passwordMax = 128;
+var charNum = "0123456789";
+var charLow = "abcdefghijklmnopqrstuvwxyz";
+var charSpecial = "!@#$%^&*()";
+var charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+function isValidLength(passwordLength) {
+  if (isNaN(passwordLength)) {
+    return false;
   }
-
-  function askSpecialCharacters() {
-    var specialCharacters = window.confirm("click ok for accepting special characters in password")
-    console.log(specialCharacters)
-    return specialCharacters;
+  if (passwordMin <= passwordLength && passwordLength <= passwordMax) {
+    return true;
   }
-
-  function askNumericCharacters() {
-    var numericConfirm = window.confirm("Click ok for accepting numeric characters in password")
-    console.log(numericConfirm)
-    return numericConfirm;
-  }
-
-  function askLowerCase() {
-    var lowerCaseConfirm = window.confirm("Click ok to verify password contains lower case letters")
-    console.log(lowerCaseConfirm)
-    return lowerCaseConfirm;
-  }
-
-  function askUpperCase() {
-    var upperCaseConfirm = window.confirm("Click ok to verify your password contains upper case letters")
-    console.log(upperCaseConfirm)
-    return upperCaseConfirm;
-  }
-
-function generatePassword() {
-
-  var finalPassword = window.confirm("Need a new password?")
-  var numberOfCharacters = askHowManyCharacters;
- 
-  for (var i=0; i<=askHowManyCharacters; i++){
-    var random = arr[Math.floor(Math.random() * arr.length)];
-  }
-
-  console.log(Array.prototype);
-
-  return finalPassword;
+  return false;
 }
 
+function getPassWordOptions() {
+  var includeUpperCase = confirm("Message");
+  var includeLowerCase = confirm("Message");
+  var includeNumeric = confirm("Message");
+  var includeSpecial = confirm("Message");
+  var passwordLength = parseInt(
+    prompt("Enter password length", passwordMin),
+    10
+  );
+  while (!isValidLength(passwordLength)) {
+    passwordLength = parseInt(
+      prompt("Not valid password length", passwordLength),
+      10
+    );
+  }
+  return {
+    upper: includeUpperCase,
+    lower: includeLowerCase,
+    numeric: includeNumeric,
+    special: includeSpecial,
+    length: passwordLength,
+  };
+}
+
+function generatePassword() {
+  var options = getPassWordOptions();
+  var result = "";
+  var characters = "";
+
+  if (options.upper) {
+    characters += charUp;
+  }
+
+  if (options.lower) {
+    characters += charLow;
+  }
+
+  if (options.special) {
+    characters += charSpecial;
+  }
+
+  if (options.numeric) {
+    characters += charNum;
+  }
+  for (var i = 0; i < options.length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) ;
+generateBtn.addEventListener("click", writePassword);
 
+// did you invoke the function? did you connect the functions to where it needs to go? Does it need to go to a new function? can you makes your functions any simpler. Can you take some of the variables in the functions and make them global? establish array element for all characters
